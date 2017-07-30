4f18c19fb34725f4febf116b55a4ab01600fbc10
Ben Charais <benjcharais@gmail.com>
2017-07-29 18:28:18 -0600

    Finished project

--- keys.js
+++ keys.js
@@ -1,8 +1,6 @@
-console.log('This is loaded');
-
 exports.twitterKeys = {
-  consumer_key: '	ThF8Jj0P09hZeD6E2PeHHNguf',
-  consumer_secret: 'YpdRE1Xk1D4nvYfEoKMHf9KZn6tPjQP6TYdtvgAxfwN0zSFWM5',
-  access_token_key: '	885668506964033537-JV7FyJnlRqMNzRAXmQhPIcCaImMkFxR',
-  access_token_secret: '	4nJxmHQVafEKbDXKsjfZoUYfJGmaFSuRafaaKXMkwJdHx'
+  consumer_key: 'ksWnn3DQnqqdjYkkzvbfzIoGL',
+  consumer_secret: 'v1hMFHENeQfPaN3tvv30vqMJFcSZpog5pl9N1LPhFXfeZ5Ybw8',
+  access_token_key: '885668506964033537-BghuHp1DXLMOiKd7td519hkFsZiygyo',
+  access_token_secret: '8I4TgCcAPo3260QFoXsmN4GhnOKa1PSFXml7Rz4K1OZ8O'
 };
--- logic.js
+++ logic.js
@@ -1,33 +1,119 @@
-const keys = require('./keys.js');
-const twitter = require('twitter')
-const spotify = require('spotify')
+const runAction = process.argv[2];
+const searchType = process.argv.slice(3).join(' ');
+const keys = require('./keys');
+const Twitter = require('twitter')
+const Spotify = require('node-spotify-api')
 const request = require('request')
 const fs = require('fs');
-
-console.log('Node is running maannnn');
-
-tweets();
-
-function tweets() {
-  var client = new twitter({
-  consumer_key: keys.consumer_key,
-  consumer_secret: keys.consumer_secret,
-  access_token_key: keys.access_token_key,
-  access_token_secret: keys.access_token_secret
+const client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+const spotify = new Spotify({
+  id: "60f5836a18a949b585e1095d7ccfebf9",
+  secret: "bec33d5c8e1d4470bd26c03b005dd8f3"
 });
 
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
+
+function chooseCommand(action, search) {
+  switch (action) {
+    case "my-tweets":
+      twitter();
+      break;
+    case "spotify-this-song":
+      spotifySong(search);
+      break;
+    case "movie-this":
+      movieThis(search);
+      break;
+    case "do-what-it-says":
+      whatItSays();
+      break;
+    default:
+      console.log(`Welcome to nowhere land traveler, care to try again?
+        Your command options are: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'`);
   }
+}
+
+function twitter() {
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for (tweet of tweets) {
+        console.log(`
+          @${tweet.user.screen_name} said:
+          ${tweet.text} on:
+          ${tweet.created_at}
+        `);
+      }
+      let now = new Date();
+      fs.appendFile('./log.txt', `${now}: Called Last 20 Tweets \n`, (err) => {
+        if (err) throw err;
+        console.log(`The call was logged to log.txt`);
+      });
+    } else {
+      console.log(error);
+    };
 });
 };
-function spotifySong() {
 
+function spotifySong(search) {
+  spotify.search({ type: 'track', query: search }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else {
+      data = data.tracks.items[0];
+      console.log(`Artist Name: ${data.artists[0].name}`);
+      console.log(`Album Name: ${data.album.name}`);
+      console.log(`Song name: ${data.name}`);
+      console.log(`For a preview click: ${data.external_urls.spotify}`);
+      let now = new Date();
+      fs.appendFile('./log.txt', `${now}: Called Spotify Music search with ${search} \n`, (err) => {
+        if (err) throw err;
+        console.log(`The call was logged to log.txt`);
+      });
+    };
+  });
 };
 
-function movieThis() {
-
+function movieThis(search) {
+  request('http://www.omdbapi.com/?t='+ (search || 'mr+nobody') + '&y=&plot=short&apikey=40e9cece',
+		function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			console.log('Title: ' + data.Title);
+			console.log('Year: ' + data.Year);
+			console.log('Rated: ' + data.Rated);
+			console.log('IMDB Rating: ' + data.imdbRating);
+			console.log('Country: ' + data.County);
+			console.log('Language: ' + data.Language);
+			console.log('Plot: ' + data.Plot);
+			console.log('Actors: ' + data.Actors);
+      let now = new Date();
+      fs.appendFile('./log.txt', `${now}: Called OMDB Movie search with ${search} \n`, (err) => {
+        if (err) throw err;
+        console.log(`The call was logged to log.txt`);
+      });
+    };
+  });
 };
+
+function whatItSays() {
+fs.readFile('./random.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    let splitData = data.split(',');
+    let action = splitData[0];
+    var search = splitData[1];
+    let now = new Date();
+    fs.appendFile('./log.txt', `${now}: Called do-what-it-says \n`, (err) => {
+      if (err) throw err;
+      console.log(`The call was logged to log.txt`);
+    })
+    spotifySong(search);
+  });
+};
+
+chooseCommand(runAction, searchType);
